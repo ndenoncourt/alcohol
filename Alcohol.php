c9c6db5b1a66526c0bd8e30860c777b02a52c6f2
Naomi <ndenoncourt@gmail.com>
2017-07-10 16:26:45 -0400

    Yesssss.

---
+++ Alcohol.php
@@ -0,0 +1,15 @@
+<?php
+namespace Alcohol;
+
+class Alcohol
+{
+    public static function load() {
+        echo 'Alcohol.'."\n";
+    }
+
+    public static function loadMore() {
+        echo 'Alcohol.'."\n";
+        echo 'Alcohol.'."\n";
+        echo 'Alcohol.'."\n";
+    }
+}
